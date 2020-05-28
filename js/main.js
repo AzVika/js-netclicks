c8a84d247b8ac83b5abea7a4224365047433d88a
prostovix <prostovix@gmail.com>
2020-05-28 19:46:02 +0300

    3 day

--- js/main.js
+++ js/main.js
@@ -1,16 +1,38 @@
 window.onload = function () {
 
-	const IMG_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2/';
-	const API_KEY = '';
+	const IMG_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2';
+	const SERVER = 'https://api.themoviedb.org/3';
+	const API_KEY = '2956e07681aeb04a3ff3a86aaf27e36f';
 	
 	// menu
-	const leftMenu = document.querySelector('.left-menu');
-	const hamburger = document.querySelector('.hamburger');
-	const tvShowsList = document.querySelector('.tv-shows__list');
-	const modal = document.querySelector('.modal');
+	const leftMenu = document.querySelector('.left-menu'),
+		hamburger = document.querySelector('.hamburger'),
+		tvShowsList = document.querySelector('.tv-shows__list'),
+		modal = document.querySelector('.modal'),
+		tvShows = document.querySelector('.tv-shows'),
+		tvCardImg = document.querySelector('.tv-card__img'),
+		modalTitle = document.querySelector('.modal__title'),
+		tvCardImgModal = document.querySelector('.modal__content .tv-card__img'),
+		genresList = document.querySelector('.genres-list'),
+		rating = document.querySelector('.rating'),
+		description = document.querySelector('.description'),
+		modalLink = document.querySelector('.modal__link'),
+		searchForm = document.querySelector('.search__form'),
+		searchFormInput = document.querySelector('.search__form-input'),
+		preloader = document.querySelector('.preloader');
+
+
+	const loading = document.createElement('div');
+	loading.className = 'loading';
 
 
 	class DBService {
+		
+		constructor () {
+			this.SERVER = 'https://api.themoviedb.org/3';
+			this.API_KEY = '2956e07681aeb04a3ff3a86aaf27e36f';
+		}
+
 		getData = async (url) => {
 			const res = await fetch(url);
 			if(res.ok) {
@@ -23,42 +45,73 @@ window.onload = function () {
 		getTestData = () => {
 			return this.getData('test.json');
 		}
+
+		getTestCard = () => {
+			return this.getData('card.json');
+		}
+
+		getSearchResult = query => {
+			return this.getData(`${this.SERVER}/search/tv?api_key=${this.API_KEY}&query=${query}&language=ru-RU`);
+		}
+
+		getTvShow = id => {
+			return this.getData(`${this.SERVER}/tv/${id}?api_key=${this.API_KEY}&language=ru-RU`);
+		}
 	}
 
 
 	const renderCard = response => {
 		tvShowsList.textContent = '';
 
-		response.results.forEach(item => {
-			const {
-				backdrop_path: backdrop,
-				name: title,
-				poster_path: poster,
-				vote_average: vote
-			} = item;
+		if(!response.total_results) {
+			tvShowsList.previousElementSibling.innerHTML = '<p>По вашему запросу ничего не найдено!</p>';
+			loading.remove();
+		} else {
+			response.results.forEach(item => {
+				const {
+					backdrop_path: backdrop,
+					name: title,
+					poster_path: poster,
+					vote_average: vote,
+					id
+				} = item;
 
-			const posterIMG = poster ? IMG_URL + poster : '../img/no-poster.jpg';
-			const backdropIMG = backdrop ? `data-backdrop="${IMG_URL + backdrop}"` : '';
-			const voteElem = vote ? `<span class="tv-card__vote">${vote}</span>` : '';
+				const posterIMG = poster ? IMG_URL + poster : '../img/no-poster.jpg';
+				const backdropIMG = backdrop ? IMG_URL + backdrop : '';
+				const voteElem = vote ? `<span class="tv-card__vote">${vote}</span>` : '';
 
-			const card = document.createElement('li');
-			card.className = 'tv-shows__item';
-			card.innerHTML = `
-				<a href="#" class="tv-card">
-                    ${voteElem}
-                    <img class="tv-card__img"
-                         src="${posterIMG}"
-                         ${backdropIMG}
-                         alt="${title}">
-                    <h4 class="tv-card__head">${title}</h4>
-                </a>
-			`;
+				const card = document.createElement('li');
+				card.className = 'tv-shows__item';
+				card.innerHTML = `
+					<a href="#" id="${id}" class="tv-card">
+	                    ${voteElem}
+	                    <img class="tv-card__img"
+	                         src="${posterIMG}"
+	                         data-backdrop="${backdropIMG}"
+	                         alt="${title}">
+	                    <h4 class="tv-card__head">${title}</h4>
+	                </a>
+				`;
 
-			tvShowsList.append(card);
-		});
-	}
+				loading.remove();
+				tvShowsList.append(card);
+			});
+		}
 
-	new DBService().getTestData().then(renderCard);
+		
+	};
+
+
+	// search from DB
+	searchForm.addEventListener('submit', event => {
+		event.preventDefault();
+		const value = searchFormInput.value.trim();
+		if (value || value >= 2) {
+			tvShows.append(loading);
+			new DBService().getSearchResult(value).then(renderCard);
+		}
+		searchFormInput.value = '';
+	});
 
 
 
@@ -78,6 +131,7 @@ window.onload = function () {
 	});
 
 	leftMenu.addEventListener('click', event => {
+		event.preventDefault();
 		const target = event.target;
 		const dropdown = target.closest('.dropdown');
 		if(dropdown) {
@@ -96,8 +150,45 @@ window.onload = function () {
 		const card = target.closest('.tv-card');
 
 		if(card) {
-			document.body.style.overflow = 'hidden';
-			modal.classList.remove('hide');
+
+			preloader.style.display = 'block';
+
+			new DBService().getTvShow(card.id)
+				.then( ({ 
+					poster_path: posterPath,
+					name: title, 
+					genres, 
+					vote_average: voteAverage,
+					overview,
+					homepage
+					}) => {
+
+					
+					tvCardImgModal.alt = title;
+					if (!posterPath) {
+						tvCardImgModal.closest('.image__content').style.display = 'none';
+					} else {
+						tvCardImgModal.src = IMG_URL + posterPath;
+					}
+
+					modalTitle.textContent = title;
+					// genresList.innerHTML = data.genres.reduce( (acc, item) => {
+					// 	return `${acc}<li>${item.name}</li>`;
+					// }, '');
+					genresList.textContent = '';
+					for(const item of genres) {
+						genresList.innerHTML += `<li>${item.name}</li>`;
+					}
+
+					rating.textContent = voteAverage;
+					description.textContent = overview;
+					modalLink.href = homepage;
+				})
+				.then( () => {
+					document.body.style.overflow = 'hidden';
+					modal.classList.remove('hide');
+					preloader.style.display = '';
+				});
 		}
 	});
 
